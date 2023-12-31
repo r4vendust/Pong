04194912054c3ea6543c06ca9b6852784dfff998
r4vendust <victor.driver@hotmail.com>
2023-12-31 01:14:52 -0300

    feat: add collision segment

--- Pong/src/Collision.cpp
+++ Pong/src/Collision.cpp
@@ -1,5 +1,10 @@
 #include "Collision.h"
 
+typedef struct ThreeSegmentRectangle
+{
+	Rectangle top, middle, bottom;
+};
+
 Collision::Collision()
 {
 	paddleCollision = false;
@@ -8,11 +13,62 @@ Collision::Collision()
 
 void Collision::CheckCollision(Paddle &paddle, CpuPaddle &cpuPaddle, Ball &ball)
 {
-	paddleCollision = CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), paddle.GetPaddle());
-	cpuPaddleCollision = CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), cpuPaddle.GetPaddle());
+	ThreeSegmentRectangle paddleSegment = ThreeSegmentCollision(paddle.GetPaddle());
+	ThreeSegmentRectangle cpuPaddleSegment = ThreeSegmentCollision(cpuPaddle.GetPaddle());
 
-	if (paddleCollision || cpuPaddleCollision)
+	DrawCollisionSegment(paddleSegment);
+	DrawCollisionSegment(cpuPaddleSegment);
+
+	DrawCircleLines(ball.GetPosition().x, ball.GetPosition().y, ball.GetRadius(), RED);
+
+	if (CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), paddleSegment.top)
+		|| CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), cpuPaddleSegment.top))
+	{
+		ball.SetSpeedY(
+			ball.GetSpeed().y * -1
+		);
+		ball.InvertSpeed();
+		std::cout << "Collision: Top" << std::endl;
+	}
+	else if (CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), paddleSegment.middle)
+		|| CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), cpuPaddleSegment.middle))
 	{
 		ball.InvertSpeed();
+		std::cout << "Collision: Middle" << std::endl;
 	}
+	else if (CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), paddleSegment.bottom)
+		|| CheckCollisionCircleRec(ball.GetPosition(), ball.GetRadius(), cpuPaddleSegment.bottom))
+	{
+		ball.SetSpeedY(
+			ball.GetSpeed().y * -1
+		);
+		ball.InvertSpeed();
+		std::cout << "Collision: Bottom" << std::endl;
+	}
+}
+
+ThreeSegmentRectangle Collision::ThreeSegmentCollision(Rectangle rect)
+{
+	float rect1Height = rect.height / 4;
+	float rect2Height = rect.height / 2;
+	float rect3Height = rect1Height;
+
+	ThreeSegmentRectangle threeSegmentRectangle;
+
+	Rectangle paddleTop = { rect.x, rect.y, rect.width, rect1Height };
+	Rectangle paddleMiddle = { rect.x, rect.y + rect1Height, rect.width + offset, rect2Height };
+	Rectangle paddleBottom = { rect.x, rect.y + rect1Height + rect2Height, rect.width, rect3Height };
+
+	threeSegmentRectangle.top = paddleTop;
+	threeSegmentRectangle.middle = paddleMiddle;
+	threeSegmentRectangle.bottom = paddleBottom;
+
+	return threeSegmentRectangle;
+}
+
+void Collision::DrawCollisionSegment(ThreeSegmentRectangle rec)
+{
+	DrawRectangleLines(rec.top.x, rec.top.y, rec.top.width, rec.top.height, RED);
+	DrawRectangleLines(rec.middle.x, rec.middle.y, rec.middle.width, rec.middle.height, GREEN);
+	DrawRectangleLines(rec.bottom.x, rec.bottom.y, rec.bottom.width, rec.bottom.height, BLUE);
 }
--- Pong/src/Collision.h
+++ Pong/src/Collision.h
@@ -3,13 +3,18 @@
 #include "CpuPaddle.h"
 #include "Ball.h"
 
+typedef struct ThreeSegmentRectangle;
+
 class Collision
 {
 public:
 	Collision();
 	void CheckCollision(Paddle &paddle, CpuPaddle &cpuPaddle, Ball &ball);
+	ThreeSegmentRectangle ThreeSegmentCollision(Rectangle rect);
+	void DrawCollisionSegment(ThreeSegmentRectangle rec);
 private:
 	bool paddleCollision;
 	bool cpuPaddleCollision;
+	const int offset = 1;
 };
 
